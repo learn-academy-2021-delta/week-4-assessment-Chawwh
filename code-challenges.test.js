ce46d6200e3d0a4b4bd2922bf1ee4cc5eba62fa4
Chawwh <chachablueftw@gmail.com>
2021-10-02 19:23:24 -0600

    It is done.

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -1,5 +1,6 @@
 // ASSESSMENT 4: JavaScript Coding Practical Questions with Jest
 
+
 // Please read all questions thoroughly
 // Pseudo coding is REQUIRED
 // If you get stuck, please leave comments to help us understand your thought process
@@ -14,36 +15,83 @@
 // --------------------1) Create a function that takes in an array, removes the first item from the array and shuffles the remaining content.
 
 // a) Create a test with an expect statement using the variable provided. HINT: Check out this resource: https://jestjs.io/docs/expect#expectarraycontainingarray
+// test created
+// Test run failed
+
+describe("colors", () =>{
+    test("a function that takes in an array, removes the first item and shuffles the remaining content", () =>{
+        expect(colors(colors1)).toEqual(expect.arrayContaining(["yellow", "blue", "pink", "green"]))
+        expect(colors(colors2)).toEqual(expect.arrayContaining(["saffron", "aquamarine", "periwinkle", "indigo", "ochre"]))
+    })
+})
 
 var colors1 = ["purple", "blue", "green", "yellow", "pink"]
 // Expected output example (can be a different order): ["yellow", "blue", "pink", "green"]
 var colors2 = ["chartreuse", "indigo", "periwinkle", "ochre", "aquamarine", "saffron"]
 // Expected output example (can be a different order): ["saffron", "aquamarine", "periwinkle", "indigo", "ochre"]
 
+// Declare a function that takes in an array
+// use .shift() to remove the first element in the array
+// Declare new variable 'shuffle' that is equal to arr.sort((a, b) => Math.floor(Math.random * arr.length))
+// this will sort the array by Math.random rounded to the nearest whole number that is less than the length of the array
+// Return shuffle
+
 
 
 // b) Create the function that makes the test pass.
-
-
+const colors = (arr) => {
+    arr.shift()
+    let shuffle = arr.sort((a, b) => Math.floor(Math.random * arr.length))
+    return shuffle
+}
+// Yay green light!
 
 // --------------------2) Create a function that takes an array of numbers and returns an array of the minimum and maximum numbers in that order.
 
 // a) Create a test with expect statements for each of the variables provided.
+describe("smToLg", () =>{
+    test("function that takes an array of numbers and returns an array of the minimum and maximum numbers in that order", () =>{
+        expect(smToLg(nums1)).toEqual([-8, 90])
+        expect(smToLg(nums2)).toEqual([5, 109])
+    })
+})
 
 var nums1 = [3, 56, 90, -8, 0, 23, 6]
 // Expected output: [-8, 90]
-var nums2 = [109, 5, 9, 67 8, 24]
+var nums2 = [109, 5, 9, 67, 8, 24]
 // Expected output: [5, 109]
 
 
 
 // b) Create the function that makes the test pass.
 
+// Declare a function that takes in an array
+// Declare a variable min and max to equal an empty array
+// push the Math.min and Math.max into minNMax using a spread operator on the array parameter
+// Return minNMax
 
+const smToLg = (arr) => {
+    let minNMax = []
+    minNMax.push(Math.min(...arr))
+    minNMax.push(Math.max(...arr))
+    return minNMax
+}
+// Yay another green light!
 
 // --------------------3) Create a function that takes in two arrays as arguments and returns one array with no duplicate values. STRETCH: Use the spread operator to pass in a dynamic number of arguments.
 
 // a) Create a test with an expect statement using the variables provided.
+
+// Test created
+// I noticed the wacky yargs and such code only appears at the top when I am using tab to autofill test declarations
+// Test run failed
+
+
+describe("noDupes", () => {
+    test("function that takes in two arrays as arguments and returns one array with no duplicate values", () =>{
+        expect(noDupes(testArray1,testArray2)).toEqual([3, 7, 10, 5, 4, 8, 2, 1])
+    })
+})
 
 var testArray1 = [3, 7, 10, 5, 4, 3, 3]
 var testArray2 = [7, 8, 2, 3, 1, 5, 4]
@@ -52,3 +100,20 @@ var testArray2 = [7, 8, 2, 3, 1, 5, 4]
 
 
 // b) Create the function that makes the test pass.
+// Alrighty so I remember this question from  a couple weeks ago because it gave me nightmares! I researched ways to solve this but didn't quite understand what was going on at the time. WELLLL I do now! It was pretty nice to go back through old work and be like Ohhh! thats what that was!  
+
+// Declare a function that takes in 2 arrays
+// declare a variable for answer that is equal to [...new Set([...arr1,...arr2 ])]
+// return answer
+
+const noDupes = (arr1,arr2) => {
+    let answer = [...new Set([...arr1,...arr2 ])]
+    return answer
+  }
+// Yay 3 green lights!!
+
+// I was messing around to see if I was just going overboard with the spread operators but I guess I'm not? It works perfectly like this?? And I don't need multiple for loops?!? *Mind happily blown*
+
+// I hope you don't mind these shorter functions! These were actually pretty fun making and refactoring... Also, Lots of arrays this week! My goodness! I feel like a pirate writing all those Arrs!
+
+// I feel alot better about this assessment! After the last assessment I found myself feeling really defeated and doing a little rough mentally. Hopefully this week turns out better! I'm happy with my code here!
--- code_challenges.rb
+++ code_challenges.rb
@@ -1,7 +1,15 @@
 # ASSESSMENT 4: Ruby Coding Practical Questions
-# MINASWAN
+# MINASWAN - Matz is nice and so we are nice *bows*
 
 # --------------------1) Create a method that takes in a number and determines if the number is even or odd. Use the test variables provided.
+
+# Psuedo Code (I don't think it's required for this part but I'm not about to get points taken off for not having it)
+# Declare a method that takes in a single integer
+# conditional if input is not an integer, log error
+# else if integer is divisible by 2, log integer is even
+# else log integer is odd
+# double end for if statement and method
+
 
 num1 = 7
 # Expected output: '7 is odd'
@@ -10,9 +18,32 @@ num2 = 42
 num3 = 221
 # Expected output: '221 is odd'
 
+def is_even int
+    if int.class != Integer
+        p 'Please enter a whole number'
+    elsif int % 2 == 0
+        p "#{int} is even."
+    else
+        p "#{int} is odd."
+    end
+end
+
+is_even num1
+is_even num2
+is_even num3
+is_even 'spaghetti'
+
+
+# I'm a little confused on how to make this method only take in integers... I'm messing around trying to get back an error when no numbers are given but it seems to be converting them somehow? Like it's trying to tell me spaghetti is even... Even if it converted the string to a number equal to the number of characters spaghetti would be odd...
+# NEVERMIND I MADE IT WORK HOW I WANTED IT TO!!! I switched my edge-case to the first if instead of the catch-all else and now it works! Hehe!
+
 
 
 # -------------------2) Create a method that takes in a string and removes all the vowels from the string. Use the test variables provided. HINT: Check out this resource: https://ruby-doc.org/core-2.6/String.html#method-i-delete
+
+# Declare a method that takes in a string
+# Log string with .delete to filter out all vowels using "aeiouAEIOU" (Gotta catch em all amirite)
+# end method
 
 album1 = 'Rubber Soul'
 # Expected output: 'Rbbr Sl'
@@ -21,9 +52,24 @@ album2 = 'Sgt Pepper'
 album3 = 'Abbey Road'
 # Expected output: 'bby Rd'
 
+def no_vowels str
+    p str.delete "aeiouAEIOU"
+end
+
+no_vowels album1
+no_vowels album2
+no_vowels album3
+no_vowels 'ThE qUiCk BrOwN fOx JuMpS oVeR tHe LaZy DoG'
+
 
 
 # -------------------3) Create a method that takes in a string and checks if the string is a palindrome. A palindrome is the same word spelled forwards or backwards. Use the test variables provided.
+
+# Declare a method that takes in a string
+# Conditional if str.downcase equals str.downcase.reverse
+# Log str is a palindrome
+# Else log str is not a palindrome
+# double end if statement and method
 
 is_palindrome1 = 'Racecar'
 # Expected output: 'Racecar is a palindrome'
@@ -31,3 +77,17 @@ is_palindrome2 = 'LEARN'
 # Expected output: 'LEARN is not a palindrome'
 is_palindrome3 = 'Rotator'
 # Expected output: 'Rotator is a palindrome'
+
+def is_palindrome str
+    if str.downcase == str.downcase.reverse
+        puts "#{str} is a palindrome!"
+    else 
+        puts "#{str} is not a palindrome!"
+    end
+end
+
+is_palindrome is_palindrome1
+is_palindrome is_palindrome2
+is_palindrome is_palindrome3
+is_palindrome 'Tacocat'
+is_palindrome 'Watashi wa rubi ga sukidesu'
